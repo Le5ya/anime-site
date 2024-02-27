aea9bcd08fa3300b70a563559e3a63a7f341e119
Le5ya <liliatolstyh@yandex.ru>
2024-02-27 23:30:51 +0300

    style correction

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,21 @@
 const modal = () => {
   const modal = document.querySelector(".search-model");
-  const modalBtn = document.querySelector(".icon_search");
+  const modalOpen = document.querySelector(".search-switch");
   const modalClose = modal.querySelector(".search-close-switch");
-  const modalInput = modal.querySelector("input");
+  const modalInput = modal.querySelector("#search-input");
 
-  modalBtn.addEventListener("click", () => {
-    modal.style.display = "block";
+  modalOpen.addEventListener("click", () => {
+    if (modal.classList.contains("active")) {
+      modal.classList.remove("active");
+    } else {
+      modal.classList.add("active");
+    }
   });
+
   modalClose.addEventListener("click", () => {
-    modal.style.display = "none";
+    modal.classList.remove("active");
   });
-  modalInput.addEventListener("click", () => {
+  modalInput.addEventListener("input", () => {
     console.log(modalInput.value);
   });
 };
